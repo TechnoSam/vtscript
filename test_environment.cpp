298f40b8193253c18930c5ea8c56b3145bdd90f6
TechnoSam <technosam4jc@gmail.com>
2017-02-19 17:25:14 -0500

    Add exists Environment tests

--- test_environment.cpp
+++ test_environment.cpp
@@ -25,54 +25,67 @@ TEST_CASE("Tests the Environment constructor", "[environment]") {
 	EnvEntry::fptrNumber multFptr = multProc;
 	EnvEntry::fptrNumber divFptr = divProc;
 
+	REQUIRE(env.exists("pi"));
 	result = env.fetch("pi");
 	REQUIRE(result.getType() == EnvEntry::Type::NUMBER);
 	REQUIRE(result.getNumber() == atan2(0, -1));
 
+	REQUIRE(env.exists("not"));
 	result = env.fetch("not");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == notFptr);
 
+	REQUIRE(env.exists("and"));
 	result = env.fetch("and");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == andFptr);
 
+	REQUIRE(env.exists("or"));
 	result = env.fetch("or");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == orFptr);
 
+	REQUIRE(env.exists("<"));
 	result = env.fetch("<");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == ltFptr);
 
+	REQUIRE(env.exists("<="));
 	result = env.fetch("<=");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == lteFptr);
 
+	REQUIRE(env.exists(">"));
 	result = env.fetch(">");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == gtFptr);
 
+	REQUIRE(env.exists(">="));
 	result = env.fetch(">=");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == gteFptr);
 
+	REQUIRE(env.exists("="));
 	result = env.fetch("=");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_BOOL);
 	REQUIRE(result.getFptrBool() == equalFptr);
 
+	REQUIRE(env.exists("+"));
 	result = env.fetch("+");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_NUMBER);
 	REQUIRE(result.getFptrNumber() == sumFptr);
 
+	REQUIRE(env.exists("-"));
 	result = env.fetch("-");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_NUMBER);
 	REQUIRE(result.getFptrNumber() == subFptr);
 
+	REQUIRE(env.exists("*"));
 	result = env.fetch("*");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_NUMBER);
 	REQUIRE(result.getFptrNumber() == multFptr);
 
+	REQUIRE(env.exists("/"));
 	result = env.fetch("/");
 	REQUIRE(result.getType() == EnvEntry::Type::FPTR_NUMBER);
 	REQUIRE(result.getFptrNumber() == divFptr);
@@ -131,6 +144,7 @@ TEST_CASE("Test invalid defining and fetching in the Environment", "[environment
 	REQUIRE_THROWS(env.define("Bool", entry));
 
 	REQUIRE_NOTHROW(env.fetch("Bool"));
+	REQUIRE(!env.exists("DNE"));
 	REQUIRE_THROWS(env.fetch("DNE"));
 
 }
